c860e64e066e94efa28626af8b81219658333c16
duza <duza@tut.by>
2018-10-28 17:01:50 +0300

    feat: Add make function which memory arguments

---
+++ src/make.js
@@ -0,0 +1,12 @@
+module.exports = function make(...args) { 
+  let allPassedArgs = [...args];
+  const recursion = (...newArgs) => {
+    if (newArgs[0] instanceof Function) {
+      return allPassedArgs.reduce(newArgs[0]); 
+    }
+    allPassedArgs = allPassedArgs.concat(newArgs);
+    return recursion;
+  }
+  return recursion;
+};
+
