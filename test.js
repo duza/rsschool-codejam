61f2a102762d0341ef5f4b5acc7de439c90fce32
duza <duza@tut.by>
2018-10-28 12:12:45 +0300

    fix: Replace incorrect method to deep equal
    Replace using incorrect equal method which unsuitable for arrays and use
    arrow functions.

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ const assert = require('assert');
 Object.freeze(assert);
 const { sumOfOther } = require('./src/index.js');
 
-describe('sumOfOther', function() {
-  describe('simple test with simple array', function() {
+describe('sumOfOther', () => {
+  describe('simple test with simple array', () => {
     const sum = sumOfOther([1, 0, 1, 0]);
     it('should return array', function() {
-      assert.equal(sum, [1, 2, 1, 2]);
+      assert.deepStrictEqual(sum, [1, 2, 1, 2]);
     })
   })
 });
