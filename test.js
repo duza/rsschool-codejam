b5bdec5f5acd793688714671e579ceea303a6e82
duza <duza@tut.by>
2018-10-28 12:25:25 +0300

    feat: Add couple of tests for sumOfOther function

--- test.js
+++ test.js
@@ -4,9 +4,19 @@ const { sumOfOther } = require('./src/index.js');
 
 describe('sumOfOther', () => {
   describe('simple test with simple array', () => {
-    const sum = sumOfOther([1, 0, 1, 0]);
-    it('should return array', function() {
+    it('should return array', () => {
+      const sum = sumOfOther([1, 0, 1, 0]);
       assert.deepStrictEqual(sum, [1, 2, 1, 2]);
-    })
-  })
+    });
+
+    it('should return empty array', () => {
+      const sum = sumOfOther([]);
+      assert.deepStrictEqual(sum, []);
+    });
+
+    it('should return array of negative numbers', () => {
+      const sum = sumOfOther([-1, -2, -3, -4, -5, -6]);
+      assert.deepStrictEqual(sum, [-20, -19, -18, -17, -16, -15]);
+    });
+  });
 });
